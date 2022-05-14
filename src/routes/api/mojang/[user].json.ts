155a774ee00fb9b37c7753a6d3e1389016ee1fd7
maverickquill55 <VScode@toxik.cf>
2022-05-14 02:42:29 -0400

    All White no Problems lets goo

--- src/routes/api/mojang/[user].json.ts
+++ src/routes/api/mojang/[user].json.ts
@@ -1,4 +1,4 @@
-export async function get({ params }:{params: { user: string } }): Promise<any> {
+export async function get({ params }:{params: { user: string } }): Promise<unknown> {
     const r = await fetch(`https://api.mojang.com/users/profiles/minecraft/${params.user}`);
     return { body: await r.body }
 }
